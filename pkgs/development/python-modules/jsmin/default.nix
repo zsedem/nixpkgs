98b28d09224219b1c20b1055cfea8e2ac086b6c9
Jonathan Ringer <jonringer117@gmail.com>
2022-01-14 10:41:29 -0800

    python3Packages.jsmin: fix tests

--- pkgs/development/python-modules/jsmin/default.nix
+++ pkgs/development/python-modules/jsmin/default.nix
@@ -1,4 +1,4 @@
-{ lib, buildPythonPackage, fetchPypi }:
+{ lib, buildPythonPackage, fetchPypi, fetchpatch }:
 
 buildPythonPackage rec {
   pname = "jsmin";
@@ -8,6 +8,14 @@ buildPythonPackage rec {
     inherit pname version;
     sha256 = "88fc1bd6033a47c5911dbcada7d279c7a8b7ad0841909590f6a742c20c4d2e08";
   };
+
+  patches = [
+    # fix tests, committed to master, but no release yet
+    (fetchpatch {
+      url = "https://github.com/tikitu/jsmin/commit/ec5e3da27634a150faf5c0456bc84ec9c428120d.patch";
+      sha256 = "sha256-TjAbxZ+zqwp2D8JremqIW6/f9NcXK1lqVoIG1hvmzco=";
+    })
+  ];
 
   meta = with lib; {
     description = "JavaScript minifier";
